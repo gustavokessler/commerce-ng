0d26cead018b5d5c618769796eab28933abc34a4
gustavokessler <37279803+gustavokessler@users.noreply.github.com>
2020-08-28 00:12:08 -0300

    cardapio lista

--- src/app/cardapio/cardapio.component.ts
+++ src/app/cardapio/cardapio.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardapioComponent implements OnInit {
 
+  filter = 'Xis';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  test(a): void{
+    console.log(a.tab.textLabel);
+    this.filter = a.tab.textLabel;
+  }
+
+  
 
 }
--- src/app/cardapio/cardapio.module.ts
+++ src/app/cardapio/cardapio.module.ts
@@ -4,12 +4,26 @@ import { CommonModule } from '@angular/common';
 import { CardapioRoutingModule } from './cardapio-routing.module';
 import { CardapioComponent } from './cardapio.component';
 
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [CardapioComponent],
   imports: [
     CommonModule,
-    CardapioRoutingModule
+    CardapioRoutingModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatGridListModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatDialogModule,
   ]
 })
 export class CardapioModule { }
